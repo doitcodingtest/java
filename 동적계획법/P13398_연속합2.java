30c4f4aa99c09942bc2d571d4b50d88f9d26b52f
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-10-31 02:33:37 +0900

    Update and rename P13398_연속된정수의합.java to P13398_연속합2.java

--- 동적계획법/P13398_연속합2.java
+++ 동적계획법/P13398_연속합2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class P13398_연속된정수의합 {
+public class P13398_연속합2 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -36,3 +36,4 @@ public class P13398_연속된정수의합 {
     br.close();
   }
 }
+
